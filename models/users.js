4cddd207cb33dcf6743f7bc8e281dcf7c19429bd
AuroreHbt <lesreveriesdelaeria@gmail.com>
2025-01-18 02:25:36 +0100

    commit test

--- models/users.js
+++ models/users.js
@@ -5,10 +5,6 @@ const userSchema = mongoose.Schema({
   email: String,
   password: String,
   token: String,
-  canCreateBook: Boolean, //par défaut = true
-  canCreateEvent: Boolean, //par défaut = true
-  canLikeBook: Boolean, //par défaut = true
-  canLikeEvent: Boolean, //par défaut = true
 });
 
 const User = mongoose.model('users', userSchema);
